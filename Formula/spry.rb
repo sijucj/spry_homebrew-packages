11800a42ff9761d0758801dd00f356c4567fd6f5
Siju Joseph <siju-joseph@netspective.in>
2025-12-08 12:17:43 +0530

    Bump Spry formula to 0.100.7

--- Formula/spry.rb
+++ Formula/spry.rb
@@ -7,10 +7,10 @@ class Spry < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/programmablemd/packages/releases/download/v0.100.7/spry-macos.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "4a987c12600e095074b5b0fe12e93810f3bda66f4d9bebc9d2ca9a25858498ae"
     else
       url "https://github.com/programmablemd/packages/releases/download/v0.100.7/spry-macos.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "4a987c12600e095074b5b0fe12e93810f3bda66f4d9bebc9d2ca9a25858498ae"
     end
   end
 
