37775e71b41dd7914cb771e893fa2b9cb93d3b0f
Siju Joseph <siju-joseph@netspective.in>
2025-12-16 09:40:10 +0530

    Bump Spry formula to 0.107.3

--- Formula/spry@0.107.3.rb
+++ Formula/spry@0.107.3.rb
@@ -1,4 +1,4 @@
-class Spry < Formula
+class SpryAT01073 < Formula
   desc "Spry CLI - A declarative web application framework"
   homepage "https://github.com/programmablemd/packages"
   version "0.107.3"
@@ -33,4 +33,3 @@ class Spry < Formula
     system "#{bin}/spry", "--version"
   end
 end
-
