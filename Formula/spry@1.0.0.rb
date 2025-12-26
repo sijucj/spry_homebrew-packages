0a34b18d830f0a38dcb584a39c775d0bf6af13a5
Siju Joseph <siju-joseph@netspective.in>
2025-12-26 16:14:39 +0530

    Bump Spry formula to 1.0.0

--- Formula/spry@1.0.0.rb
+++ Formula/spry@1.0.0.rb
@@ -4,15 +4,8 @@ class SpryAT100 < Formula
   version "1.0.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry-macos.tar.gz"
-      sha256 "bc311a136ada152d97de9a107f7b6b783f15a87261e8cc41101c2a76d1c66234"
-    else
-      url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry-macos.tar.gz"
-      sha256 "bc311a136ada152d97de9a107f7b6b783f15a87261e8cc41101c2a76d1c66234"
-    end
-  end
+  url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry-macos.tar.gz"
+  sha256 "bc311a136ada152d97de9a107f7b6b783f15a87261e8cc41101c2a76d1c66234"
 
   on_linux do
     url "https://github.com/programmablemd/packages/releases/download/v1.0.0/spry_1.0.0-ubuntu22.04u1_amd64.deb"
